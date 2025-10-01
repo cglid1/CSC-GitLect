8c74da3cc3103a136a4810adf04b7776fee0bede
cglid1 <colinglidden77@gmail.com>
2025-10-01 12:26:12 -0400

    corrected john's smell

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
-public class main {
+public class Main {
     public static void main() {
         System.out.println("John smells, like a dog");
+        System.out.println("John is a dog");
     }
 }
